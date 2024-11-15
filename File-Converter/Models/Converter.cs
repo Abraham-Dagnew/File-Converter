2bebe6b06319ac57c195cb459d0c2a3576f422b8
Abraham Dagnew <Ahaymero@gmail.com>
2024-11-14 23:48:37 -0500

    add code to Converter class

--- File-Converter/Models/Converter.cs
+++ File-Converter/Models/Converter.cs
@@ -1,7 +1,41 @@
-﻿namespace File_Converter.Models
+﻿using Aspose.Words;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace File_Converter.Services
 {
     public class Converter
     {
+        public async Task<string> ConvertAsync(IFormFile file, string conversionType)
+        {
+            string uploadDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+            Directory.CreateDirectory(uploadDir);
 
+            string filePath = Path.Combine(uploadDir, file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            string convertedFilePath = Path.ChangeExtension(filePath, conversionType == "WordToPDF" ? ".pdf" : ".docx");
+
+            if (conversionType == "WordToPDF")
+            {
+                var doc = new Document(filePath);
+                doc.Save(convertedFilePath, SaveFormat.Pdf);
+            }
+            else if (conversionType == "PDFToWord")
+            {
+                var doc = new Document(filePath);
+                doc.Save(convertedFilePath, SaveFormat.Docx);
+            }
+            else
+            {
+                return null; 
+            }
+
+            return convertedFilePath;
+        }
     }
 }
