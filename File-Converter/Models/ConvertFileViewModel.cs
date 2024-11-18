36a8a5cf069dadb0d817696da35d33be4bea9f78
Abraham Dagnew <Ahaymero@gmail.com>
2024-11-17 22:50:52 -0500

    Download librelibrary to convert pdf to word and installed Freeware to change pdf to word

---
+++ File-Converter/Models/ConvertFileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace File_Converter.Models
+{
+    public class ConvertFileViewModel
+    {
+        public IFormFile File { get; set; }
+        public string ConversionType { get; set; }
+    }
+}
--- File-Converter/Models/Converter.cs
+++ File-Converter/Models/Converter.cs
@@ -1,7 +1,69 @@
-﻿namespace Converter
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Freeware;  
+
+namespace File_Converter.Services
 {
     public class Converter
     {
+        public async Task<string> ConvertPdfToWord(string filePath, string outputDir)
+        {
+            Directory.CreateDirectory(outputDir);
 
+            try
+            {
+                byte[] pdfBytes = File.ReadAllBytes(filePath);
+
+                using (MemoryStream pdfStream = new MemoryStream(pdfBytes))
+                {
+                    byte[] docxBytes = Pdf2Docx.Convert(pdfStream);  
+
+                    string outputFilePath = Path.Combine(outputDir, Path.GetFileNameWithoutExtension(filePath) + ".docx");
+                    await File.WriteAllBytesAsync(outputFilePath, docxBytes);
+
+                    return outputFilePath;  
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error while converting PDF to DOCX: " + ex.Message);
+            }
+        }
+
+        public async Task<string> ConvertWordToPdf(string filePath, string outputDir)
+        {
+            string libreOfficePath = @"C:\Program Files\LibreOffice\program\soffice.exe";
+
+            Directory.CreateDirectory(outputDir);
+
+            string arguments = $"--headless --convert-to pdf --outdir \"{outputDir}\" \"{filePath}\"";
+
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = libreOfficePath,
+                    Arguments = arguments,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                };
+
+                using (var process = Process.Start(processStartInfo))
+                {
+                    await process.WaitForExitAsync();
+                }
+
+                string convertedFilePath = Path.Combine(outputDir, Path.GetFileNameWithoutExtension(filePath) + ".pdf");
+                return convertedFilePath;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error while converting Word to PDF: " + ex.Message);
+            }
+        }
     }
 }
