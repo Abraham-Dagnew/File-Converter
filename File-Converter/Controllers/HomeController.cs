fead11730bcc50f059cd2cf48777c86570a36261
Abraham Dagnew <Ahaymero@gmail.com>
2024-11-18 23:54:44 -0500

    Updated HC

--- File-Converter/Controllers/HomeController.cs
+++ File-Converter/Controllers/HomeController.cs
@@ -1,32 +1,74 @@
-using File_Converter.Models;
+using File_Converter.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace File_Converter.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly Converter _converter;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(Converter converter)
         {
-            _logger = logger;
+            _converter = converter;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConvertFile(IFormFile file, string conversionType)
+        {
+            if (file == null || file.Length == 0)
+            {
+                ViewBag.Message = "Please select a file to upload.";
+                return View("Index");
+            }
+
+            try
+            {
+                string uploadDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+                Directory.CreateDirectory(uploadDir);
+                string filePath = Path.Combine(uploadDir, file.FileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                string outputDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "converted");
+
+                string convertedFilePath = string.Empty;
+
+                if (conversionType == "PDFToWord")
+                {
+                    convertedFilePath = await _converter.ConvertPdfToWord(filePath, outputDir);
+                }
+                else if (conversionType == "WordToPDF")
+                {
+                    convertedFilePath = await _converter.ConvertWordToPdf(filePath, outputDir);
+                }
+
+                if (string.IsNullOrEmpty(convertedFilePath))
+                {
+                    ViewBag.Message = "Conversion failed.";
+                    return View("Index");
+                }
+
+                var fileBytes = System.IO.File.ReadAllBytes(convertedFilePath);
+                var fileDownloadName = Path.GetFileName(convertedFilePath);
+                return File(fileBytes, "application/octet-stream", fileDownloadName);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "An error occurred while converting the file.";
+                return View("Index");
+            }
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
