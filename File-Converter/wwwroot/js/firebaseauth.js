f0b6b2bd3ff382fafc05dd6401c085327e2f993f
Abraham Dagnew <Ahaymero@gmail.com>
2024-11-27 20:13:53 -0500

    Added firebase api keys

---
+++ File-Converter/wwwroot/js/firebaseauth.js
@@ -0,0 +1,16 @@
+﻿import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDcQLnjMyAE5mgRh0idXqmNHmwmiHIZvrY",
+    authDomain: "file-converter-a81ec.firebaseapp.com",
+    projectId: "file-converter-a81ec",
+    storageBucket: "file-converter-a81ec.firebasestorage.app",
+    messagingSenderId: "560076138645",
+    appId: "1:560076138645:web:367d2a5867fb3a4beb539c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
